139733ea18fa02dd4848b04bd2b1d871ee620a50
Mudit Gupta <mudit_gupta87@yahoo.com>
2019-05-16 21:10:21 +0530

    Create dbd.php
    :MG:DBD

---
+++ account/dbd.php
@@ -0,0 +1,12 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$database = 'u333976797_jovia';
+$user = 'u333976797_lpix';
+$pass = 'H4PFxLkHfnqT2ygHrH';
+$host = 'localhost';
+$dir = dirname(__FILE__) . '/jovialpix.sql';
+echo "<h3>Backing up database to `<code>{$dir}</code>`</h3>";
+exec("mysqldump --user={$user} --password={$pass} --host={$host} {$database} --result-file={$dir} 2>&1", $output);
+var_dump($output);
